6d3a6ecd812c13737718070ee12e2c847cf37628
Sergey <sergey86@gmail.com>
2023-04-29 20:36:50 +0200

    Added fonts, html markup, spinner

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -30,7 +30,7 @@ export const AppProvider = () => {
     <React.Suspense
       fallback={
         <div className="flex items-center justify-center w-screen h-screen">
-          <Spinner size="xl" />
+          <Spinner />
         </div>
       }
     >
