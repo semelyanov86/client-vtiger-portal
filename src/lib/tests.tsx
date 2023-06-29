b2b0765f708b906f3a694ef500dfdc692ffca42b
sergeiemelianov <rDmz9cEvF7>
2023-06-29 16:38:32 +0200

    removed unused functions

--- src/lib/tests.tsx
+++ src/lib/tests.tsx
@@ -1,4 +1,3 @@
-import { render } from '@testing-library/react';
 import { ReactNode } from 'react';
 import { IntlProvider } from 'react-intl';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -33,23 +32,3 @@ const createTestQueryClient = () =>
       },
     },
   });
-
-export function renderWithClient(ui: React.ReactElement) {
-  const testQueryClient = createTestQueryClient();
-  const { rerender, ...result } = render(
-    <QueryClientProvider client={testQueryClient}>{ui}</QueryClientProvider>
-  );
-  return {
-    ...result,
-    rerender: (rerenderUi: React.ReactElement) =>
-      rerender(<QueryClientProvider client={testQueryClient}>{rerenderUi}</QueryClientProvider>),
-  };
-}
-
-export function createQueryWrapper() {
-  const testQueryClient = createTestQueryClient();
-  // eslint-disable-next-line react/display-name
-  return ({ children }: { children: React.ReactNode }) => (
-    <QueryClientProvider client={testQueryClient}>{children}</QueryClientProvider>
-  );
-}
