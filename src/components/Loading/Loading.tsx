d775d22745a1696500c0f7cab54d4bfa93bc1b52
Sergey <sergey86@gmail.com>
2023-07-02 20:13:15 +0200

    Removed repeatness of spinner

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,26 +1,5 @@
-import { useEffect } from 'react';
+import { Spinner } from '../Elements';
 
 export const Loading = () => {
-  useEffect(() => {
-    const contentArea = document.querySelector('#contentArea') as HTMLElement;
-    const htmlTag = document.documentElement;
-    if (!contentArea) {
-      htmlTag.setAttribute('data-show', 'false');
-    } else {
-      contentArea.setAttribute('style', 'opacity:0');
-    }
-    document.body.classList.add('spinner');
-    return () => {
-      setTimeout(() => {
-        if (!contentArea) {
-          htmlTag.setAttribute('data-show', 'true');
-        } else {
-          contentArea.setAttribute('style', 'opacity:1');
-        }
-        document.body.classList.remove('spinner');
-      }, 30);
-    };
-  }, []);
-
-  return <></>;
+  return <Spinner />;
 };
